f386090fd1a1bd39599cb7faf079d44aafa7313e
Yevhenii <misterjuk2005@gmail.com>
2025-02-24 02:50:26 +0100

    Implemented RotatorComponent

---
+++ Minigin/RotatorComponent.cpp
@@ -0,0 +1,43 @@
+﻿#define _USE_MATH_DEFINES
+
+#include "RotatorComponent.h"
+#include "GameObject.h"
+#include "TransformComponent.h"
+#include "Time.h"
+#include <cmath>   
+#include <stdexcept>
+
+namespace dae 
+{
+    RotatorComponent::RotatorComponent(GameObject* owner)
+        : Component(owner)
+    {
+        // Cache the TransformComponent pointer
+        if (auto transformComponent = owner->GetComponent<TransformComponent>())
+        {
+            m_TransformComponent = transformComponent;
+            m_TargetPosition = m_TransformComponent->GetLocalPosition();
+        }
+        else
+        {
+            // Handle error: TransformComponent not found
+            throw std::runtime_error("TransformComponent not found on the GameObject");
+        }
+    }
+
+    void RotatorComponent::Update()
+    {
+        if (m_Owner == nullptr || m_TransformComponent == nullptr) 
+            return;
+       
+        m_Angle += m_Speed * Time::GetInstance().GetDeltaTime();
+
+        // Keep angle within [0, 2pi] to avoid overflow
+        m_Angle = fmod(m_Angle, 2 * static_cast<float>(M_PI));
+
+        glm::vec3 position;
+        position.x = m_TargetPosition.x + m_Radius * std::cos(m_Angle);
+        position.y = m_TargetPosition.y + m_Radius * std::sin(m_Angle);
+        m_TransformComponent->SetLocalPosition(position);
+    }
+}
---
+++ Minigin/RotatorComponent.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Component.h"
+#include <glm.hpp>
+
+namespace dae 
+{
+
+	class TransformComponent;
+
+	class RotatorComponent final : public Component
+	{
+	public:
+
+		RotatorComponent(GameObject* owner);
+		~RotatorComponent() = default;
+
+		void Update() override;
+
+		void SetRadius(const float radius) { m_Radius = radius; };
+		void SetSpeed(const float speed) { m_Speed = speed; };
+
+	private:
+		float m_Radius{20.0f}; 
+		float m_Speed{1.0f};  
+
+		float m_Angle{};
+
+		glm::vec3 m_TargetPosition{};
+
+		TransformComponent* m_TransformComponent;
+	};
+
+}
+
